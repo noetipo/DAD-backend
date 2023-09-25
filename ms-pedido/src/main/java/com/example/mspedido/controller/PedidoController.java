e047956e44d741219d63047c07091d1ec0bd7926
noetipo <noe.tipo@gmail.com>
2023-09-25 10:00:39 -0500

    resiliencia

--- ms-pedido/src/main/java/com/example/mspedido/controller/PedidoController.java
+++ ms-pedido/src/main/java/com/example/mspedido/controller/PedidoController.java
@@ -2,6 +2,7 @@ package com.example.mspedido.controller;
 
 import com.example.mspedido.entity.Pedido;
 import com.example.mspedido.service.PedidoService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,10 +29,9 @@ public class PedidoController {
     public ResponseEntity<Pedido> update(@RequestBody Pedido pedido) {
         return ResponseEntity.ok(pedidoService.actualizar(pedido));
     }
-
+    @CircuitBreaker(name = "pedidoListarPorIdCB", fallbackMethod = "fallBackPedidoListarPorIdCB")
     @GetMapping("/{id}")
     public ResponseEntity<Pedido> listById(@PathVariable(required = true) Integer id) {
-        System.out.println("Pedido+ " + id);
         return ResponseEntity.ok().body(pedidoService.listarPorId(id).get());
     }
 
@@ -40,4 +40,10 @@ public class PedidoController {
         pedidoService.eliminarPorId(id);
         return ResponseEntity.ok(pedidoService.listar());
     }
+    private ResponseEntity<Pedido> fallBackPedidoListarPorIdCB(@PathVariable(required = true) Integer id, RuntimeException e) {
+        Pedido pedido = new Pedido();
+        pedido.setId(90000);
+        return ResponseEntity.ok().body(pedido);
+    }
+
 }
